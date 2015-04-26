ea3e4c2e501d958a70a9a80b166bfac218e8b78c
me.askai <me.askai@gmail.com>
2015-04-26 18:16:21 +0300

    27/04/15      Создал фрагмент с логотипом, зарегал отдельный класс и связал с главной активностью/ Создал макет логотипа /временно изменил иконку приложения  26.04.2015  Создал пакет с фрагментами. Подготовил слой для фрагмента, описывающий шапку приложения (лого, лич. каб. и т.д.)  26.04.2015  Изменил мин. вресию СДК на API14

--- Iris/app/src/main/java/ru/tentacles/iris/fragments/headerLogoFragment.java
+++ Iris/app/src/main/java/ru/tentacles/iris/fragments/headerLogoFragment.java
@@ -1,5 +1,6 @@
 package ru.tentacles.iris.fragments;
 
+<<<<<<< HEAD
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import ru.tentacles.iris.R;
+=======
+        import android.os.Bundle;
+        import android.support.annotation.Nullable;
+        import android.support.v4.app.Fragment;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+
+        import ru.tentacles.iris.R;
+>>>>>>> 27/04/15      Создал фрагмент с логотипом, зарегал отдельный класс и связал с главной активностью/ Создал макет логотипа /временно изменил иконку приложения
 
 
 //класс, который будет привязан к фрагменту header_logo
@@ -19,4 +30,8 @@ public class headerLogoFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
         return inflater.inflate(R.layout.header_logo, container, true);
     }
+<<<<<<< HEAD
+=======
+
+>>>>>>> 27/04/15      Создал фрагмент с логотипом, зарегал отдельный класс и связал с главной активностью/ Создал макет логотипа /временно изменил иконку приложения
 }
