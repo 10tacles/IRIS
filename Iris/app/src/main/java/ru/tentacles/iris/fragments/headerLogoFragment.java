0d0782255f33f16438852bd61ce45ea546e95a65
me.askai <me.askai@gmail.com>
2015-04-29 23:36:06 +0300

    29.04.15 Сдедали стили, поколдовали над кнопками 28.04.15 Назвали иконки 27.04.15 Сделал 2 фрагмента для главной активности. Скомпоновал view-элементы. Сделал макет лого

--- Iris/app/src/main/java/ru/tentacles/iris/fragments/headerLogoFragment.java
+++ Iris/app/src/main/java/ru/tentacles/iris/fragments/headerLogoFragment.java
@@ -1,6 +1,6 @@
 package ru.tentacles.iris.fragments;
 
-<<<<<<< HEAD
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,16 +9,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import ru.tentacles.iris.R;
-=======
-        import android.os.Bundle;
-        import android.support.annotation.Nullable;
-        import android.support.v4.app.Fragment;
-        import android.view.LayoutInflater;
-        import android.view.View;
-        import android.view.ViewGroup;
 
-        import ru.tentacles.iris.R;
->>>>>>> 27/04/15      Создал фрагмент с логотипом, зарегал отдельный класс и связал с главной активностью/ Создал макет логотипа /временно изменил иконку приложения
+
+
+
 
 
 //класс, который будет привязан к фрагменту header_logo
@@ -30,8 +24,5 @@ public class headerLogoFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
         return inflater.inflate(R.layout.header_logo, container, true);
     }
-<<<<<<< HEAD
-=======
 
->>>>>>> 27/04/15      Создал фрагмент с логотипом, зарегал отдельный класс и связал с главной активностью/ Создал макет логотипа /временно изменил иконку приложения
 }
